d5406b3585a24a6e72dbf68aa00eda6aba8643fe
Jarrod <57122151+JLCode-tech@users.noreply.github.com>
2025-07-25 12:27:49 +1000

    Create variables.tf

---
+++ variables.tf
@@ -0,0 +1,11 @@
+variable "workspace_name" {
+  description = "Name of the workspace"
+  type        = string
+  default     = "test-workspace"
+}
+
+variable "environment" {
+  description = "Environment name"
+  type        = string
+  default     = "development"
+}
